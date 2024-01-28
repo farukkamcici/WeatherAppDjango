90e93f49b6a5160327ad1e62967c6c5e7110e710
Faruk Kamçıcı <farukkamcici@gmail.com>
2024-01-28 18:47:59 +0300

    async home response created

--- weatherProject/weatherapp/utils.py
+++ weatherProject/weatherapp/utils.py
@@ -1,8 +1,148 @@
 import pytz
 from datetime import datetime
+import requests
+import pytz
+from datetime import datetime,timedelta
+import aiohttp
 
 
-def timezone_conv(last_upd_loc, loc_tz_str):
+
+
+
+async def get_city_info(city):
+    api_key = "7ed79061f55449a1a76205741242401"
+    url = f"http://api.weatherapi.com/v1/current.json?key={api_key}&q={city}"
+    
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as data:
+            response = await data.json()
+
+    
+    name = response["location"]["name"]
+    country= response["location"]["country"]
+    curr_temp_c = round(response["current"]["temp_c"])
+    curr_cond_icon = response["current"]["condition"]["icon"]
+    curr_cond_desc = response["current"]["condition"]["text"]
+    last_upd_loc = response["current"]["last_updated"]
+    loc_tz_str=response["location"]["tz_id"]
+    last_update = await timezone_conv(last_upd_loc, loc_tz_str)
+    humidity=response["current"]["humidity"]
+    wind=response["current"]["wind_kph"]
+    feelslike=response["current"]["feelslike_c"]
+
+
+    return {
+        "name": name,
+        "country": country,
+        "curr_temp_c": curr_temp_c,
+        "curr_cond_icon": curr_cond_icon,
+        "curr_cond_desc": curr_cond_desc,
+        "last_update": last_update,
+        "humidity": humidity,
+        "wind": wind,
+        "feelslike": feelslike,
+
+    }
+
+def get_city_info_hourly(city):
+    api_key = "7ed79061f55449a1a76205741242401"
+    url = f"http://api.weatherapi.com/v1/forecast.json?key={api_key}&q={city}&days=3"
+    response = requests.get(url).json()  
+
+    def target_days_list():
+        local_timezone = pytz.timezone('Europe/Moscow')
+        today = datetime.now(tz=local_timezone)
+        tomorrow=today+ timedelta(days=1)
+        after_tomorrow=today+ timedelta(days=2)
+
+        today_str = today.strftime('%Y-%m-%d')
+        tomorrow_str = tomorrow.strftime('%Y-%m-%d')
+        after_tomorrow_str = after_tomorrow.strftime('%Y-%m-%d')
+
+        target_days=[today_str,tomorrow_str,after_tomorrow_str]
+        
+        return target_days
+    
+    target_days=target_days_list()
+    target_hours_list = ["03:00", "12:00", "21:00"]
+    result= {}
+
+    entry_dict = {}
+
+    for i, day in enumerate(target_days):
+        day_data = next((data for data in response['forecast']['forecastday'] if data['date'] == str(day)), None)
+
+        if day_data:
+            result[str(day)] = {
+                'date': day,
+                'hours': {}
+            }
+            for j, hour in enumerate(target_hours_list):
+                date_hour = f"{day} {hour}"
+                target_hour_data = next((time for time in day_data['hour'] if time['time'] == date_hour), None)
+                #print(target_hour_data)
+
+                if target_hour_data:
+                    entry_data = {
+                        f"temperature{i}{j}": target_hour_data['temp_c'],
+                        f"text{i}{j}": target_hour_data['condition']['text'],
+                        f"icon{i}{j}": target_hour_data['condition']['icon'],
+                    }
+                    entry_dict.update(entry_data)
+                else:
+                    print(f"No data found for {hour} on {day}")
+        else:
+            print(f"No data found for {day}")
+
+    #print(entry_dict)
+    return entry_dict
+
+
+def get_city_info_days(city):
+    api_key = "7ed79061f55449a1a76205741242401"
+    url = f"http://api.weatherapi.com/v1/forecast.json?key={api_key}&q={city}&days=3"
+    response = requests.get(url).json()  
+    
+    
+
+    def target_days_list():
+        local_timezone = pytz.timezone('Europe/Moscow')
+        today = datetime.now(tz=local_timezone)
+        tomorrow=today+ timedelta(days=1)
+        after_tomorrow=today+ timedelta(days=2)
+
+        today_str = today.strftime('%Y-%m-%d')
+        tomorrow_str = tomorrow.strftime('%Y-%m-%d')
+        after_tomorrow_str = after_tomorrow.strftime('%Y-%m-%d')
+
+        target_days=[today_str,tomorrow_str,after_tomorrow_str]
+        
+        return target_days
+    
+    target_days=target_days_list()
+    result = {}
+
+    for i, target_day in enumerate(target_days):
+        target_day_data = next((day for day in response['forecast']['forecastday'] if day['date'] == str(target_day)),None)
+        target_day_dict = target_day_data.get('day', {})
+        date_str=target_day_data["date"]
+        date_obj=datetime.strptime(date_str, '%Y-%m-%d')
+        formatted_date=date_obj.strftime('%d %b %a')
+
+        if target_day_dict:
+            result[f"max_temp_{i + 1}"] = target_day_dict["maxtemp_c"]
+            result[f"min_temp_{i + 1}"] = target_day_dict["mintemp_c"]
+            result[f"avg_temp_{i + 1}"] = target_day_dict["avgtemp_c"]
+            result[f"rain_chance_{i + 1}"] = target_day_dict["daily_chance_of_rain"]
+            result[f"avghumidity_{i + 1}"] = target_day_dict["avghumidity"]
+            result[f"date_{i+1}"]=formatted_date
+            result[f"text_{i + 1}"] = target_day_dict["condition"]["text"]
+            result[f"icon_{i + 1}"] = target_day_dict["condition"]["icon"]
+        
+    return result
+
+
+async def timezone_conv(last_upd_loc, loc_tz_str):
     date_format = '%Y-%m-%d %H:%M'
     last_upd_dt = datetime.strptime(last_upd_loc, date_format)
 
@@ -13,4 +153,4 @@ def timezone_conv(last_upd_loc, loc_tz_str):
     last_update = last_upd_dt_loc.astimezone(new_tz)
     last_update_conv_str = last_update.strftime(date_format)
 
-    return last_update_conv_str
+    return  last_update_conv_str
--- weatherProject/weatherapp/views.py
+++ weatherProject/weatherapp/views.py
@@ -1,167 +1,59 @@
-from collections.abc import Callable, Iterable, Mapping
-from typing import Any
 from django.shortcuts import render,redirect
 from django.urls import reverse_lazy
-import requests
-import pytz
-from datetime import datetime,timedelta
 from django.contrib.auth import login, authenticate 
 from .forms import SignUpForm
-from threading import Thread
-from .utils import timezone_conv
-
-        
-def get_city_info(city):
-    api_key = "7ed79061f55449a1a76205741242401"
-    url = f"http://api.weatherapi.com/v1/current.json?key={api_key}&q={city}"
-    response = requests.get(url).json()  
-    
-    name = response["location"]["name"]
-    country= response["location"]["country"]
-    curr_temp_c = round(response["current"]["temp_c"])
-    curr_cond_icon = response["current"]["condition"]["icon"]
-    curr_cond_desc = response["current"]["condition"]["text"]
-    last_upd_loc = response["current"]["last_updated"]
-    loc_tz_str=response["location"]["tz_id"]
-    last_update = timezone_conv(last_upd_loc, loc_tz_str)
-    humidity=response["current"]["humidity"]
-    wind=response["current"]["wind_kph"]
-    feelslike=response["current"]["feelslike_c"]
+from .utils import get_city_info, get_city_info_days, get_city_info_hourly
+import asyncio
+from django.template.response import TemplateResponse
+import time
 
 
-    return {
-        "name": name,
-        "country": country,
-        "curr_temp_c": curr_temp_c,
-        "curr_cond_icon": curr_cond_icon,
-        "curr_cond_desc": curr_cond_desc,
-        "last_update": last_update,
-        "humidity": humidity,
-        "wind": wind,
-        "feelslike": feelslike,
 
-    }
+async def async_get_city_info(city):
+    loop = asyncio.get_event_loop()
+    return await loop.run_in_executor(None, lambda: get_city_info(city))
 
-def get_city_info_hourly(city):
-    api_key = "7ed79061f55449a1a76205741242401"
-    url = f"http://api.weatherapi.com/v1/forecast.json?key={api_key}&q={city}&days=3"
-    response = requests.get(url).json()  
+async def async_get_city_info_days(city):
+    loop = asyncio.get_event_loop()
+    return await loop.run_in_executor(None, lambda: get_city_info_days(city))
 
-    def target_days_list():
-        local_timezone = pytz.timezone('Europe/Moscow')
-        today = datetime.now(tz=local_timezone)
-        tomorrow=today+ timedelta(days=1)
-        after_tomorrow=today+ timedelta(days=2)
-
-        today_str = today.strftime('%Y-%m-%d')
-        tomorrow_str = tomorrow.strftime('%Y-%m-%d')
-        after_tomorrow_str = after_tomorrow.strftime('%Y-%m-%d')
-
-        target_days=[today_str,tomorrow_str,after_tomorrow_str]
-        
-        return target_days
-    
-    target_days=target_days_list()
-    target_hours_list = ["03:00", "12:00", "21:00"]
-    result= {}
-
-    entry_dict = {}
-
-    for i, day in enumerate(target_days):
-        day_data = next((data for data in response['forecast']['forecastday'] if data['date'] == str(day)), None)
-
-        if day_data:
-            result[str(day)] = {
-                'date': day,
-                'hours': {}
-            }
-            for j, hour in enumerate(target_hours_list):
-                date_hour = f"{day} {hour}"
-                target_hour_data = next((time for time in day_data['hour'] if time['time'] == date_hour), None)
-                #print(target_hour_data)
-
-                if target_hour_data:
-                    entry_data = {
-                        f"temperature{i}{j}": target_hour_data['temp_c'],
-                        f"text{i}{j}": target_hour_data['condition']['text'],
-                        f"icon{i}{j}": target_hour_data['condition']['icon'],
-                    }
-                    entry_dict.update(entry_data)
-                else:
-                    print(f"No data found for {hour} on {day}")
-        else:
-            print(f"No data found for {day}")
-
-    #print(entry_dict)
-    return entry_dict
+async def async_get_city_info_hourly(city):
+    loop = asyncio.get_event_loop()
+    return await loop.run_in_executor(None, lambda: get_city_info_hourly(city))
 
 
-def get_city_info_days(city):
-    api_key = "7ed79061f55449a1a76205741242401"
-    url = f"http://api.weatherapi.com/v1/forecast.json?key={api_key}&q={city}&days=3"
-    response = requests.get(url).json()  
-    
-    
+async def home(request):
+    st= time.time()
 
-    def target_days_list():
-        local_timezone = pytz.timezone('Europe/Moscow')
-        today = datetime.now(tz=local_timezone)
-        tomorrow=today+ timedelta(days=1)
-        after_tomorrow=today+ timedelta(days=2)
-
-        today_str = today.strftime('%Y-%m-%d')
-        tomorrow_str = tomorrow.strftime('%Y-%m-%d')
-        after_tomorrow_str = after_tomorrow.strftime('%Y-%m-%d')
-
-        target_days=[today_str,tomorrow_str,after_tomorrow_str]
-        
-        return target_days
-    
-    target_days=target_days_list()
-    result = {}
-
-    for i, target_day in enumerate(target_days):
-        target_day_data = next((day for day in response['forecast']['forecastday'] if day['date'] == str(target_day)),None)
-        target_day_dict = target_day_data.get('day', {})
-        date_str=target_day_data["date"]
-        date_obj=datetime.strptime(date_str, '%Y-%m-%d')
-        formatted_date=date_obj.strftime('%d %b %a')
-
-        if target_day_dict:
-            result[f"max_temp_{i + 1}"] = target_day_dict["maxtemp_c"]
-            result[f"min_temp_{i + 1}"] = target_day_dict["mintemp_c"]
-            result[f"avg_temp_{i + 1}"] = target_day_dict["avgtemp_c"]
-            result[f"rain_chance_{i + 1}"] = target_day_dict["daily_chance_of_rain"]
-            result[f"avghumidity_{i + 1}"] = target_day_dict["avghumidity"]
-            result[f"date_{i+1}"]=formatted_date
-            result[f"text_{i + 1}"] = target_day_dict["condition"]["text"]
-            result[f"icon_{i + 1}"] = target_day_dict["condition"]["icon"]
-        
-    return result
-
-
-def home(request):
     cities = ["Istanbul", "London", "New York", "Madrid", "Rome", "Paris"]
     
     city_data = []
-    for city in cities:
-        city_info = get_city_info(city)
-        city_info_days = get_city_info_days(city)
-        city_info_hours = get_city_info_hourly(city)
 
+    tasks_info = [async_get_city_info(city) for city in cities]
+    tasks_days = [async_get_city_info_days(city) for city in cities]
+    tasks_hourly = [async_get_city_info_hourly(city) for city in cities]
+
+    results_info = await asyncio.gather(*tasks_info)
+    results_days = await asyncio.gather(*tasks_days)
+    results_hourly = await asyncio.gather(*tasks_hourly)
+
+    for  info, days, hourly in zip( results_info, results_days, results_hourly):
         city_dict = {
-            "city_info": city_info,
-            "city_info_days": city_info_days,
-            "city_info_hours": city_info_hours,
+            "city_info": info,
+            "city_info_days": days,
+            "city_info_hours": hourly,
         }
-
         city_data.append(city_dict)
 
     context = {
         "city_data": city_data
     }
 
-    return render(request, "weatherapp/home.html", context)
+    et= time.time()
+    elap_time=et-st
+    print(elap_time)
+    return  TemplateResponse(request, "weatherapp/home.html", context)
+
 
         
 
